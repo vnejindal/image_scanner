bf553713b23bf1ebc477f13529f6fe5a38e9f335
Vinay Jindal <vinay.jindal@gmail.com>
2017-03-04 11:58:45 +0530

    Working copy of image scanner app

---
+++ src/imagecrop.py
@@ -0,0 +1,186 @@
+"""
+Mobile Image Scanner 
+
+Dependencies: 
+Python : 2.7
+OpenCV : Version 2.4.13 
+    -- downloaded from: http://opencv.org/
+    -- install instructions: 
+       http://stackoverflow.com/questions/4709301/installing-opencv-on-windows-7-for-python-2-7
+
+Following module files were downloaded first and then installed using pip:
+Source: http://www.lfd.uci.edu/~gohlke/pythonlibs
+Modules: numpy, scikit_image, scipy 
+
+pip installed modules: 
+======================
+appdirs (1.4.2)
+cycler (0.10.0)
+dask (0.14.0)
+decorator (4.0.11)
+functools32 (3.2.3.post2)
+imutils (0.3.10)
+matplotlib (2.0.0)
+networkx (1.11)
+numpy (1.12.0+mkl)
+olefile (0.44)
+packaging (16.8)
+paho-mqtt (1.2)
+Pillow (4.0.0)
+pip (9.0.1)
+pyparsing (2.1.10)
+python-dateutil (2.6.0)
+pytz (2016.10)
+PyWavelets (0.5.1)
+scikit-image (0.13.dev0)
+scipy (0.18.1)
+setuptools (34.3.0)
+six (1.10.0)
+toolz (0.8.2)
+wheel (0.29.0)
+
+Courtesy: 
+http://www.pyimagesearch.com/2014/09/01/build-kick-ass-mobile-document-scanner-just-5-minutes/
+
+"""
+
+
+
+import imutils
+from skimage.filters import threshold_adaptive
+import numpy as np
+import argparse
+import cv2
+
+########################################################################
+def order_points(pts):
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype = "float32")
+    
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis = 1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis = 1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    
+    # return the ordered coordinates
+    return rect
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+    
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    
+    # return the warped image
+    return warped
+
+########################################################################
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True,
+    help = "Path to the image to be scanned")
+args = vars(ap.parse_args())
+# load the image and compute the ratio of the old height
+# to the new height, clone it, and resize it
+image = cv2.imread(args["image"])
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+image = imutils.resize(image, height = 500)
+
+# convert the image to grayscale, blur it, and find edges
+# in the image
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+
+# show the original image and the edge detected image
+print "STEP 1: Edge Detection"
+
+cv2.imshow("Image", image)
+cv2.imshow("Edged", edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# find the contours in the edged image, keeping only the
+# largest ones, and initialize the screen contour
+
+
+(cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+
+screenCnt = 0
+# loop over the contours
+for c in cnts:
+    # approximate the contour
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+    # if our approximated contour has four points, then we
+    # can assume that we have found our screen
+    if len(approx) == 4:
+        screenCnt = approx
+        break
+
+# show the contour (outline) of the piece of paper
+print "STEP 2: Find contours of paper"
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+cv2.imshow("Outline", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# apply the four point transform to obtain a top-down
+# view of the original image
+warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+
+# convert the warped image to grayscale, then threshold it
+# to give it that 'black and white' paper effect
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+warped = threshold_adaptive(warped, 251, offset = 10)
+warped = warped.astype("uint8") * 255
+
+# show the original and scanned images
+print "STEP 3: Apply perspective transform"
+cv2.imshow("Original", imutils.resize(orig, height = 650))
+cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+cv2.waitKey(0)
